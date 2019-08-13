7459b8b287ba1cb166482732b727600d67ce91f8
Agoreal <41769347+agoreal@users.noreply.github.com>
2019-08-13 18:16:39 +0800

    [ add ] field

--- orange.js
+++ orange.js
@@ -2,14 +2,14 @@
 ============================================
 *  Description: 觅诚项目组件库
 *  Author: 张功睿
-*  Version: 1.0.0
+*  Version: 1.0.4
 *  Edit: 2019-08-12 10:49
-*  LastEdit: 2019-08-13 14:30
+*  LastEdit: 2019-08-13 18:16
 ============================================
 */
 var templates = {}
 
-templates.oHeader = '\
+templates.oHeader = ['\
   <header>\
     <div class="o-header-left">\
       <span\
@@ -32,8 +32,8 @@ templates.oHeader = '\
       </span>\
     </div>\
   </header>\
-'
-templates.oTabCard = '\
+']
+templates.oTabCard = ['\
   <div class="tab-card flex-x-a">\
     <div\
       class="tab-card-tab flex-y-c"\
@@ -57,9 +57,8 @@ templates.oTabCard = '\
       </div >\
     </div>\
   </div>\
-'
-
-templates.oButton = '\
+']
+templates.oButton = ['\
   <button\
     class="o-button"\
     :class="[\'o-button--\' + color, \'o-button--\' + size, {\
@@ -76,10 +75,66 @@ templates.oButton = '\
     </span>\
     <lable class="o-button-text"><slot></slot></lable>\
   </button>\
-'
+']
+templates.oField = ['\
+  <div\
+    class="o-field"\
+    :class="[{\
+      \'is-textarea\': type === \'textarea\',\
+      \'is-nolabel\': !label\
+    }]"\
+  >\
+    <label v-if="label">{{ label }}</label>\
+    <div class="o-field-value">\
+      <textarea\
+        class="o-field-core o-field-textarea"\
+        @change="$emit(\'change\', currentValue)"\
+        :value="currentValue"\
+        ref="textarea"\
+        :placeholder="placeholder"\
+        :disabled="disabled"\
+        :readonly="readonly"\
+        :rows="rows"\
+        v-model="currentValue"\
+        v-if="type === \'textarea\'"\
+      ></textarea >\
+      <input\
+        class="o-field-core"\
+        @change="$emit(\'change\', currentValue)"\
+        @input="handleInput"\
+        ref="input"\
+        :value="currentValue"\
+        :type="type"\
+        :placeholder="placeholder"\
+        :disabled="disabled"\
+        :readonly="readonly"\
+        v-else>\
+      <div\
+        class="o-field-clear"\
+        @click="handleClear"\
+        v-if="!disableClear"\
+        v-show="currentValue && type !== \'textarea\'"\
+      >\
+        <i class="iconfont icon-guanbi"></i>\
+      </div >\
+      <span\
+        class="o-field-state"\
+        :class="[\'is-\' + state]"\
+        v-if="state"\
+      >\
+        <i class="iconfont" :class="[{\
+          \'icon-wuuiconxuan\': state === \'success\',\
+          \'icon-guanbi\': state === \'error\',\
+          \'icon-jinggao\': state === \'warning\'\
+        }]"></i>\
+      </span>\
+      <div class="o-field-other"><slot></slot></div>\
+    </div>\
+  </div >\
+']
 
 Vue.component('o-header', {
-  template: templates.oHeader,
+  template: templates.oHeader[0],
   props: {
     header: Object,
     back: Boolean
@@ -92,14 +147,14 @@ Vue.component('o-header', {
 })
 
 Vue.component('o-tab-card', {
-  template: templates.oTabCard,
+  template: templates.oTabCard[0],
   props: {
     tabs: Object
   }
 })
 
 Vue.component('o-button', {
-  template: templates.oButton,
+  template: templates.oButton[0],
   props: {
     disabled: Boolean,
     icon: String,
@@ -129,6 +184,71 @@ Vue.component('o-button', {
   methods: {
     handleClick: function (event) {
       this.$emit('click', event)
+    }
+  }
+})
+
+Vue.component('o-field', {
+  template: templates.oField[0],
+  data: function () {
+    return {
+      currentValue: this.value
+    }
+  },
+  props: {
+    label: String,
+    rows: String,
+    type: {
+      type: String,
+      default: 'text'
+    },
+    placeholder: String,
+    readonly: Boolean,
+    disabled: Boolean,
+    disableClear: Boolean,
+    state: {
+      type: String,
+      default: 'default',
+      validator: function (value) {
+        return [
+          'success',
+          'error',
+          'warning'
+        ].indexOf(value) > -1
+      }
+    },
+    value: String,
+    attr: Object
+  },
+  methods: {
+    handleInput: function (event) {
+      this.currentValue = event.target.value
+    },
+    handleClear: function () {
+      if (this.disabled || this.readonly) return
+      this.currentValue = ''
+    }
+  },
+  watch: {
+    value: function (val) {
+      this.currentValue = val
+    },
+    currentValue: function (val) {
+      this.$emit('input', val)
+    },
+    attr: {
+      immediate: true,
+      handler: function (attrs) {
+        this.$nextTick(function () {
+          var target = [this.$refs.input, this.$refs.textarea]
+          target.forEach(function (el) {
+            if (!el || !attrs) return
+            Object.keys(attrs).map(function (name) {
+              return el.setAttribute(name, attrs[name])
+            })
+          })
+        })
+      }
     }
   }
 })
